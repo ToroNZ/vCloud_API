fe4c4cd8712183b950688779eafd07fb6fdab6c3
Tomas <tomasmaggio@gmail.com>
2016-10-18 09:28:35 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,10 +11,10 @@ DEB_PACKAGE_NAME = "python python-pip"
 
 if [[ ! -z $YUM_CMD ]]; then
 	yum install -y $YUM_PACKAGE_NAME
-  pip install inquirer
+	pip install inquirer
 elif [[ ! -z $APT_GET_CMD ]]; then
 	apt-get install -y $DEB_PACKAGE_NAME
-  pip install inquirer
+	pip install inquirer
 elif [[ ! -z $OTHER_CMD ]]; then
 	$OTHER_CMD <proper arguments>
 else
